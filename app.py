9c8b5bf231c2d64211cf2b1a70dacde65a132a81
Jomon Raju <78149482+jomon-r@users.noreply.github.com>
2021-07-13 21:45:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+st.title('Spam Ham Deployment')
+test_model = joblib.load('/content/drive/MyDrive/ML MAY BATCH MAJOR PROJECT/Sentiment-Analysis')
+ip = st.text_input('Enter your message')
+op = test_model.predict([ip])
+if op[0] == '1':
+  result = 'POSITIVE COMMENT'
+elif op[0] == '0':
+  result = 'NEUTRAL COMMENT'
+else:
+  result = 'NEGATIVE COMMENT'
+if st.button('Predict'):
+  st.title(result)
