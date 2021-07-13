232d902483f50e597baffbeeed1bf8d8e71714d9
Jomon Raju <78149482+jomon-r@users.noreply.github.com>
2021-07-13 21:57:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-st.title('Spam Ham Deployment')
+st.title('Sentiment Analysis Deployment')
 test_model = joblib.load('Sentiment-Analysis')
 ip = st.text_input('Enter your message')
 op = test_model.predict([ip])
